8dc20801a3177a38ce9a9e59a68161f2ac7147aa
Gizot <tastangizem@gmail.com>
2023-07-11 15:07:39 +0300

    created data

--- src/app/category/category.module.ts
+++ src/app/category/category.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoryContentComponent } from './category-content/category-content.component';
 import { CategoryListComponent } from './category-list/category-list.component';
+import { FormsModule } from '@angular/forms';
+import { AppModule } from '../app.module';
+import { RouterModule } from '@angular/router';
 
 
 
@@ -11,7 +14,10 @@ import { CategoryListComponent } from './category-list/category-list.component';
     CategoryListComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    AppModule,
+    RouterModule,
   ]
 })
 export class CategoryModule { }
---
+++ src/assets/defaultCategories.ts
@@ -0,0 +1,27 @@
+export const defaultcategories = [
+    {
+      categoryId: 1,
+      name: "Plants",
+      creationDate: "2017-08-02"
+    },
+    {
+      categoryId: 2,
+      name: "Travel",
+      creationDate: "2020-03-03"
+    },
+    {
+      categoryId: 3,
+      name: "Music",
+      creationDate: "2017-09-22"
+    },
+    {
+      categoryId: 4,
+      name: "Film",
+      creationDate: "2016-07-16"
+    },
+    {
+      categoryId: 5,
+      name: "Book",
+      creationDate: "2019-11-22"
+    }
+  ]
