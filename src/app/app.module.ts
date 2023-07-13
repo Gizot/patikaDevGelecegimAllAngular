0f473afa724da2c2faf9fe226fc38de2f6f78443
Gizot <tastangizem@gmail.com>
2023-07-13 07:25:51 +0300

    app module modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CategoryModule } from './category/category.module';
-import { PostAddingComponent } from './post/post-adding/post-adding.component';
-import { PostContentComponent } from './post/post-content/post-content.component';
-import { PostListComponent } from './post/post-list/post-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostAddingComponent,
-    PostContentComponent,
-    PostListComponent,
   ],
   imports: [
     BrowserModule,
