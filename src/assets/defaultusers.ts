400864697d2232e713646e0dc0bac6543b343e80
Gizot <tastangizem@gmail.com>
2023-07-13 07:25:17 +0300

    preparing to navbar component

---
+++ src/assets/defaultusers.ts
@@ -0,0 +1,73 @@
+export const defaultusers = [
+    {
+      userId: 1,
+      userName: "Tsunade",
+      email: "hokage@splazma.com",
+      creationDate: "2018-08-17",
+      isActive: false
+    },
+    {
+      userId: 2,
+      userName: "Naruto",
+      email: "esterabum@g.com",
+      creationDate: "2018-03-01",
+      isActive: true
+    },
+    {
+      userId: 3,
+      userName: "Tobi",
+      email: "stelandstrong@edu.tr",
+      creationDate: "2019-12-31",
+      isActive: true
+    },
+    {
+      userId: 4,
+      userName: "Sasori",
+      email: "lefanz@unblog.fr",
+      creationDate: "2018-09-04",
+      isActive: false
+    },
+    {
+      userId: 5,
+      userName: "Kakashi",
+      email: "dsxd@sing.org",
+      creationDate: "2017-04-17",
+      isActive: false
+    },
+    {
+      userId: 6,
+      userName: "EroSennin",
+      email: "dellerker5@hugedomains.com",
+      creationDate: "2019-11-10",
+      isActive: false
+    },
+    {
+      userId: 7,
+      userName: "SakuraChan",
+      email: "dclavering6@imdb.com",
+      creationDate: "2019-02-16",
+      isActive: true
+    },
+    {
+      userId: 8,
+      userName: "Connan",
+      email: "kobalt7@cisco.com",
+      creationDate: "2015-08-09",
+      isActive: false
+    },
+    {
+      userId: 9,
+      userName: "Hidan",
+      email: "cxd@net.com",
+      creationDate: "2015-11-19",
+      isActive: true
+    },
+    {
+      userId: 10,
+      userName: "Deidara",
+      email: "hinata@bloglovin.com",
+      creationDate: "2020-03-07",
+      isActive: true
+    }
+  ]
+
